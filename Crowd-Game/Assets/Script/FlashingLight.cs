c38b8a561bca6d0e6947ba16fcb9f6862955a541
KaneTesta <42565992+KaneTesta@users.noreply.github.com>
2019-07-02 17:23:29 +1000

    Flashing Lights Added

---
+++ Crowd-Game/Assets/Script/FlashingLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+    Light testLight;
+	
+	void Start () {
+		testLight = GetComponent<Light>();
+		StartCoroutine(Flashing());
+	}
+	
+	IEnumerator Flashing ()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(0.25f);
+			testLight.enabled = ! testLight.enabled;
+
+		}
+	}
+}
